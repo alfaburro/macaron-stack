8ba7815752cd8f014b1f86761a97c3dad72ff396
Mathieu Strauch <ms@creemedia.com>
2023-08-25 20:24:43 +0000

    fastly install cli

---
+++ feathers-basics/fastly-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+url="https://github.com/fastly/cli/releases/download/v10.2.4/fastly_v10.2.4_linux-amd64.tar.gz"
+tmp_dir=$(mktemp -d)
+tmp_name="fastly-"$(date +"%s")
+curl -fsSL $url > "${tmp_dir}/${tmp_name}.tar.gz"
+tar -C ${tmp_dir} -xzf "${tmp_dir}/${tmp_name}.tar.gz"
+chmod 0755 "${tmp_dir}/fastly"
+dir_bin="/usr/bin"
+binary="${dir_bin}/fastly"
+sudo mv "${tmp_dir}/fastly" "${binary}" 
