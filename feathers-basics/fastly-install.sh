6ae1577a620179b474bbe60782843df80f4e3202
Mathieu Strauch <ms@creemedia.com>
2023-08-25 20:52:05 +0000

    mkdir fastly

--- feathers-basics/fastly-install.sh
+++ feathers-basics/fastly-install.sh
@@ -8,7 +8,7 @@ curl -fsSL $url > "${tmp_dir}/${tmp_name}.tar.gz"
 tar -C ${tmp_dir} -xzf "${tmp_dir}/${tmp_name}.tar.gz"
 chmod 0755 "${tmp_dir}/fastly"
 mkdir ${PLATFORM_APP_DIR}/.fastly
-mkdir ${PLATFORM_APP_DIR}/.config
+mkdir ${PLATFORM_APP_DIR}/.config/fastly
 binary="${PLATFORM_APP_DIR}/.fastly/fastly"
 cp "${tmp_dir}/fastly" "${binary}"
 PATH=$PATH:/app/.fastly
