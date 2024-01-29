23e46ccbc4772d33ba712ae51ef8d3b5c643ec63
Mathieu Strauch <ms@creemedia.com>
2024-01-29 14:34:13 +0000

    mkdir bin

--- remix/fastly-install.sh
+++ remix/fastly-install.sh
@@ -8,5 +8,6 @@ curl -fsSL $url > "${tmp_dir}/${tmp_name}.tar.gz"
 tar -C ${tmp_dir} -xzf "${tmp_dir}/${tmp_name}.tar.gz"
 chmod 0755 "${tmp_dir}/fastly"
 dir_bin="./bin"
+mkdir bin
 binary="${dir_bin}/fastly"
 mv "${tmp_dir}/fastly" "${binary}" 
